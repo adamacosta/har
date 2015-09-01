fadfac94a909621a1e3ec32610568a025b3fb3b8
adamacosta <acosta.adam@gmail.com>
2015-09-01 16:00:37 -0500

    First commit

---
+++ R/har.R
@@ -0,0 +1,34 @@
+#' Bank data
+#'
+#' All columns labeled [x|y|z][1-4] indicate acceleration in the indicated
+#' dimension as measured by an accelerometer. Accelerometer 1 is mounted on the
+#' waist, accelerometer 2 is mounted on the left thight, accelerometer 3 is
+#' mounted on the right ankle, and accelerometer 4 is mounted on the right
+#' upper-arm.
+#'
+#' @source Human Activity Recognition Project,
+#'  \url{http://groupware.les.inf.puc-rio.br/har#ixzz2PyRdbAfA}
+#' @format Data frame with columns
+#' \describe{
+#' \item{user}{character Name of study participant}
+#' \item{gender}{character Gender of study participant}
+#' \item{age}{integer}
+#' \item{how_tall_in_meters}{numeric}
+#' \item{weight}{integer}
+#' \item{body_mass_index}{numeric}
+#' \item{x1}{integer}
+#' \item{y1}{integer}
+#' \item{z1}{integer}
+#' \item{x2}{integer}
+#' \item{y2}{integer}
+#' \item{z2}{integer}
+#' \item{x3}{integer}
+#' \item{y3}{integer}
+#' \item{z3}{integer}
+#' \item{x4}{integer}
+#' \item{y4}{integer}
+#' \item{z4}{integer}
+#' }
+#' @examples
+#'   data(har)
+"har"
---
+++ data-raw/har.R
@@ -0,0 +1,18 @@
+library(dplyr)
+
+url <- paste0('http://groupware.les.inf.puc-rio.br/static/har/',
+              'dataset-har-PUC-Rio-ugulino.zip')
+
+zipFile <- file.path('data-raw', 'har.zip')
+datFile <- file.path('data-raw', 'dataset-har-PUC-Rio-ugulino.csv')
+outFile <- file.path('data', 'har.rda')
+
+if (!file.exists(outFile)) {
+    download.file(url, destfile = zipFile)
+    unzip(zipFile, exdir = 'data-raw')
+    system(paste('rm -f', zipFile))
+    har <- read.table(datFile, header = TRUE, sep = ';') %>%
+        tbl_df()
+    save(har, file = outFile)
+    system(paste('rm -f', datFile))
+}
